c04ca54fb8ab96f8a1729889819f33a18e074dba
nelsonenriquez <nenriquezp@miumg.edu.gt>
2023-03-25 10:07:13 -0600

    Actualizando código

--- Colecciones/src/main/java/gt/edu/umg/progra3/parcial1/DinamicStructuresManager.java
+++ Colecciones/src/main/java/gt/edu/umg/progra3/parcial1/DinamicStructuresManager.java
@@ -1,30 +1,3 @@
-/*package gt.edu.umg.progra3.parcial1;
-
-import java.nio.charset.CoderMalfunctionError;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class DinamicStructuresManager {
-
-    List<T> values = new ArrayList<>();
-
-    public DinamicStructuresManager(List<T> values) {
-        this.values = values;
-    }
-
-    public List<T> ordenar(){
-        //inserte codigo aqui
-        return null;
-    }
-
-    public List<T> ordenarInversa(){
-        //inserte codigo aqui
-        return values;
-    }
-
-}*/
-
 package gt.edu.umg.progra3.parcial1;
 
 import java.util.ArrayList;
--- Colecciones/src/main/java/gt/edu/umg/progra3/parcial1/StaticStructureManager.java
+++ Colecciones/src/main/java/gt/edu/umg/progra3/parcial1/StaticStructureManager.java
@@ -1,32 +1,3 @@
-/*package gt.edu.umg.progra3.parcial1;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-public class StaticStructureManager {
-
-    T[] values ;
-
-    public StaticStructureManager(T[] values) {
-        this.values = values;
-    }
-
-    public T[] ordenar(){
-        //inserte codigo aqui
-        Arrays.sort(values);
-        return values;
-    }
-
-    public T[] ordenarInversa(){
-        //inserte codigo aqui
-
-        return null;
-    }
-
-}
-*/
 package gt.edu.umg.progra3.parcial1;
 
         import java.util.ArrayList;
